24a4cd43fdb6f35d67e52b85d74deb124aff6785
mabiTLM <mabitlm@naver.com>
2023-09-21 15:13:58 +0900

    수업내용 커밋

--- 20230921_0/scripts/main.js
+++ 20230921_0/scripts/main.js
@@ -15,3 +15,16 @@ let getLi = document.querySelector(".m1");
 console.log(getLi);
 
 getLi.innerHTML="변경해볼까?";
+
+
+/*
+프론트를 할때 알아야 하는것
+
+브라우저 렌더링 과정
+DOM(document object model)
+BOM
+
+var,let,const
+함수, 블록, 레벨스코프
+
+*/
